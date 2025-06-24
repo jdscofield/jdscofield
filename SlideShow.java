434cef6779d151d55a6a5f7d7293dfcf71c3a3a5
jdscofield <168022825+jdscofield@users.noreply.github.com>
2025-06-23 23:00:33 -0400

    Add files via upload
    Initial Java application

---
+++ SlideShow.java
@@ -0,0 +1,173 @@
+import java.awt.BorderLayout;
+import java.awt.CardLayout;
+import java.awt.EventQueue;
+import java.awt.FlowLayout;
+import java.awt.HeadlessException;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import java.awt.Color;
+
+public class SlideShow extends JFrame {
+
+	//Declare Variables
+	private JPanel slidePane;
+	private JPanel textPane;
+	private JPanel buttonPane;
+	private CardLayout card;
+	private CardLayout cardText;
+	private JButton btnPrev;
+	private JButton btnNext;
+	private JLabel lblSlide;
+	private JLabel lblTextArea;
+
+	/**
+	 * Create the application.
+	 */
+	public SlideShow() throws HeadlessException {
+		initComponent();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initComponent() {
+		//Initialize variables to empty objects
+		card = new CardLayout();
+		cardText = new CardLayout();
+		slidePane = new JPanel();
+		textPane = new JPanel();
+		textPane.setBackground(Color.BLUE);
+		textPane.setBounds(5, 470, 790, 50);
+		textPane.setVisible(true);
+		buttonPane = new JPanel();
+		btnPrev = new JButton();
+		btnNext = new JButton();
+		lblSlide = new JLabel();
+		lblTextArea = new JLabel();
+
+		//Setup frame attributes
+		setSize(800, 600);
+		setLocationRelativeTo(null);
+		setTitle("Top 5 Destinations SlideShow");
+		getContentPane().setLayout(new BorderLayout(10, 50));
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		//Setting the layouts for the panels
+		slidePane.setLayout(card);
+		textPane.setLayout(cardText);
+		
+		//logic to add each of the slides and text
+		for (int i = 1; i <= 5; i++) {
+			lblSlide = new JLabel();
+			lblTextArea = new JLabel();
+			lblSlide.setText(getResizeIcon(i));
+			lblTextArea.setText(getTextDescription(i));
+			slidePane.add(lblSlide, "card" + i);
+			textPane.add(lblTextArea, "cardText" + i);
+		}
+
+		getContentPane().add(slidePane, BorderLayout.CENTER);
+		getContentPane().add(textPane, BorderLayout.SOUTH);
+
+		buttonPane.setLayout(new FlowLayout(FlowLayout.CENTER, 20, 10));
+
+		btnPrev.setText("Previous");
+		btnPrev.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				goPrevious();
+			}
+		});
+		buttonPane.add(btnPrev);
+
+		btnNext.setText("Next");
+		btnNext.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				goNext();
+			}
+		});
+		buttonPane.add(btnNext);
+
+		getContentPane().add(buttonPane, BorderLayout.SOUTH);
+	}
+
+	/**
+	 * Previous Button Functionality
+	 */
+	private void goPrevious() {
+		card.previous(slidePane);
+		cardText.previous(textPane);
+	}
+	
+	/**
+	 * Next Button Functionality
+	 */
+	private void goNext() {
+		card.next(slidePane);
+		cardText.next(textPane);
+	}
+
+	/**
+	 * Method to get the images
+	 */
+	private String getResizeIcon(int i) {
+		String image = ""; 
+		//Changed the parameter of the getResources method to point to the new vacation images
+		if (i==1){
+			image = "<html><body><img width= '800' height='500' src='" + getClass().getResource("/resources/Cruise.jpg") + "'</body></html>";
+		} else if (i==2){
+			image = "<html><body><img width= '800' height='500' src='" + getClass().getResource("/resources/Maldives.jpg") + "'</body></html>";
+		} else if (i==3){
+			image = "<html><body><img width= '800' height='500' src='" + getClass().getResource("/resources/Queensland.JPG") + "'</body></html>";
+		} else if (i==4){
+			image = "<html><body><img width= '800' height='500' src='" + getClass().getResource("/resources/Christmas_Island.jpg") + "'</body></html>";
+		} else if (i==5){
+			image = "<html><body><img width= '800' height='500' src='" + getClass().getResource("/resources/Juneau.jpg") + "'</body></html>";
+		}
+		return image;
+	}
+	
+	/**
+	 * Method to get the text values
+	 */
+	private String getTextDescription(int i) {
+		String text = ""; 
+		/*
+		 * Changed the contents of the text body, having one title with font size 5 
+		 * and a separate description beneath it
+		 */
+		if (i==1){
+			text = "<html><body><font size='5'>#1 Mediterranean Cruise.</font> <br>Enjoy a relaxing cruise through the Mediterranean.</body></html>";
+		} else if (i==2){
+			text = "<html><body><font size='5'>#2 Maldives Resort.</font> <br>Treat yourself to a restful vacation in the Maldives.</br></body></html>";
+		} else if (i==3){
+			text = "<html><body><font size='5'>#3 Queensland Australia Resort.</font> <br>Unwind at this tropical Queensland Resort.</br></body></html>";
+		} else if (i==4){
+			text = "<html><body><font size='5'>#4 Christmas Island</font> <br>Find the perfect getaway in remote Christmas Island. </br></body></html>";
+		} else if (i==5){
+			text = "<html><body><font size='5'>#5 Juneau Cabin</font> <br>Experience the calm of a forest retreat.</br></body></html>";
+		}
+		return text;
+	}
+	
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+
+			@Override
+			public void run() {
+				SlideShow ss = new SlideShow();
+				ss.setVisible(true);
+			}
+		});
+	}
+}
